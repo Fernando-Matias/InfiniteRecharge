b6455fc33614dc44cc81070f9aa84ceafa9128e8
Ragib Sakib <ssjsaki@gmail.com>
2020-02-22 11:47:21 -0800

    Add files via upload

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -53,8 +53,8 @@ public final class Constants {
     public static final int kTimeoutms = 10;
 
     //PIDTurn
-    public static final double kPIDTurn_P = 0.2;
-    public static final double kPIDTurn_I = 0;
-    public static final double kPIDTurn_D = 0;
+    public static final double kTurn_P = 0.2;
+    public static final double kTurn_I = 0;
+    public static final double kTurn_D = 0;
     public static final double kToleranceDegrees = 2.0;
 }
