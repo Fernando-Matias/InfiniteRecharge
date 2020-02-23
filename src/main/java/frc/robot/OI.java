242eb65e814b0e8b2bad3cde120565ac17f5fe45
Ragib Sakib <ssjsaki@gmail.com>
2020-02-23 12:35:58 -0800

    Add files via upload

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -19,7 +19,7 @@ public class OI {
     private static final LogitechController Gamepad = new LogitechController(RobotMap.mGamepadPort);
 
     public void registerControls(){
-      Gamepad.getButtonA().whenPressed(new RotateTo0());
+      Gamepad.getButtonA().whenPressed(new SpinPort8());
       Gamepad.getButtonB().whenPressed(new Rotateto90());
       Gamepad.getButtonY().whenPressed(new Rotateto180());
       Gamepad.getButtonX().whenPressed(new Rotateto270());
